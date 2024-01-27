97b0c72130a68b66b2671bd21a8feb8c0cda70e9
Luke <56170386+luke0545@users.noreply.github.com>
2024-01-27 14:26:33 -0700

    init

---
+++ Habitual-iOS/Habitual_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habitual_iOSApp.swift
+//  Habitual-iOS
+//
+//  Created by Luke on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Habitual_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
